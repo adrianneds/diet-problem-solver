1ad63e53787b9b82b49003b0aaeb761e77a0066a
Adrianne <solisadrianned17@gmail.com>
2025-02-02 17:37:27 +0800

    Add files via upload

---
+++ app.R
@@ -0,0 +1,322 @@
+#####     CMSC 150 FINAL PROJECT         #####
+#####     Author: Adrianne D. Solis      #####
+#####     Section: B1L                   #####
+#####     Diet Problem Solver:  Main     #####
+
+# install packages that are not yet installed
+packages = c("shiny", "bslib", "shinyWidgets", "readxl")
+
+for (i in 1:length(packages)) {
+  if (!(packages[i] %in% rownames(installed.packages()))) {
+    install.packages(packages[i])
+  }
+}
+
+library(shiny); library(bslib); library(shinyWidgets); library(readxl);
+
+# [0] Run simplex function definitions
+source("simplex_solver.R")
+
+vboxGreen = value_box_theme(
+  bg = '#629946',
+  fg = 'white',
+)
+
+vboxOrange = value_box_theme(
+  bg = '#df9321',
+  fg = 'white',
+)
+
+# [2] UI structure
+ui <- page_sidebar(
+
+  style = 'background-image: url(foodbg6.png); background-repeat:no-repeat',
+  title = div(
+    class = "d-flex align-items-center justify-content-between",
+    h3("Diet Problem Solver", style="font-family:Trebuchet MS; font-weight:bold; color:white"),# Title
+    div(
+      actionButton("about_btn", "About the Program",
+                   style="color:white; font-family:Trebuchet MS; background-color:#6f325c;
+                   padding:10px; border-color: #6f325c; font-size:16px"),
+      style="padding-left:35px"
+    ),
+    div(
+      actionButton("dev_btn", "Developer",
+                   style="color:white; font-family:Trebuchet MS; background-color:#6f325c;
+                   padding:10px; border-color: #6f325c; font-size:16px"),
+      style="padding-left:20px"
+    ),
+    div(
+      actionButton("how_btn", "How to Use?",
+                   style="color:white; font-family:Trebuchet MS; background-color:#6f325c;
+                   padding:10px; border-color: #6f325c; font-size:16px"),
+      style="padding-left:20px"
+    )
+  ),
+
+  layout_columns(
+
+    # arranged in a single row
+    value_box(
+     title = 'Number of Iterations',
+     id = 'iter',
+     value = span(textOutput('iterVal'), style = "font-size: 50px; font-weight: bold; font-family: Trebuchet MS"),
+     theme = vboxGreen,
+     max_height = 130,
+     showcase = bsicons::bs_icon('clipboard2-check-fill')
+    ),
+
+    value_box(
+      title = 'Daily Cost',
+      id = 'cost',
+      value = span(textOutput('costVal'), style = "font-size: 50px; font-weight: bold; font-family: Trebuchet MS"),
+      theme = vboxOrange,
+      max_height = 130,
+      showcase = bsicons::bs_icon("currency-dollar")
+    ),
+
+    value_box(
+      title = 'Number of Food Items',
+      id = 'foods',
+      value = span(textOutput('foodVal'), style = "font-size: 50px; font-weight: bold; font-family: Trebuchet MS"),
+      theme = vboxGreen,
+      max_height = 130,
+      showcase = bsicons::bs_icon("basket-fill")
+    ),
+
+  ), # end of layout_columns
+
+  layout_columns(
+
+    card(
+      span(textOutput("optCost"), style='color: #9d1a75; font-size: 25px; font-style:italic; font-family:Trebuchet MS'),
+      id = "final_values",
+      span(textOutput("final_values_txt"),     style="font-style:italic; font-weight:bold; font-family: Trebuchet MS"),
+      span(textOutput("infeasible_txt"),     style="font-size:25px; font-style:italic; font-weight:bold; font-family: Trebuchet MS"),
+      tableOutput("final_table"),
+      full_screen = TRUE,
+      height=250
+    ),
+
+  ), # end of layout_columns
+
+  layout_columns(
+    card(
+      span(textOutput("ini_tableau_title"), style="font-style:italic; font-weight:bold; font-family: Trebuchet MS"),
+      div(tableOutput("ini_table"),style="font-size:80%"),
+
+      span(textOutput("fin_tableau_title"), style="font-style:italic; font-weight:bold; font-family: Trebuchet MS"),
+      div(tableOutput("fin_table"),style="font-size:80%"),
+
+      span(textOutput("basic_sol_title"),  style="font-style:italic; font-weight:bold; font-family: Trebuchet MS"),
+      div(tableOutput("basic_table"),style="font-size:80%"),
+
+      span(textOutput("simplex_text"),     style="font-style:italic; font-weight:bold; font-family: Trebuchet MS"),
+      div(tableOutput("simplex_outputs"),style="font-size:70%"),
+      full_screen = TRUE,
+      height=250
+    )
+  ), # end of layout_columns
+
+    sidebar = sidebar(
+      bg = "#994e82",
+      fg = "#FFF",
+      width = 300,
+      title = "Select food items for your daily diet",
+      checkboxGroupInput(inputId = "checkbox_group", "",
+                         choiceNames = foodNamesList,
+                         choiceValues = foodValuesList ),
+
+      # select/unselect all
+      actionButton(inputId = "select_all", "Select/Unselect All",style="background-color:#FFF; color:#9d1a75; font-family: Trebuchet MS"),
+
+      # submit button
+      actionButton(inputId = "submit", "Find my optimal diet!",style="background-color:#FFF; color:#9d1a75; font-family: Trebuchet MS"),
+
+      # Select test case (note: for testing purposes only)
+      actionButton(inputId = "test", "Sample Output; Select first 20 food items", style="background-color:#FFF; color:#9d1a75; font-family: Trebuchet MS")
+    ), # end of sidebar
+
+  tags$head(tags$style(HTML(
+    '#submit:hover, #test:hover {
+        background-color: #629946 !important;
+        color: white !important;
+        font-weight:bold; font-style:italic;
+      }
+    #select_all:hover {
+        background-color: #df9321 !important;
+        color: white !important;
+    }
+    .navbar-static-top {
+        background-color:#994e82 !important;
+    }
+    h1.bslib-page-title.navbar-brand {
+        color: white !important;
+        font-weight: bold !important;
+    }
+    #about_btn:hover, #dev_btn:hover, #how_btn:hover {
+        background-color: #bc4e9a !important;
+        color: white !important;
+    }'
+  )))
+
+) # end of page_sidebar
+
+modal_abt <-modalDialog(
+
+  h6("About the Program", style="font-weight:bold"),
+  p("The diet problem solver gives users an optimal daily diet that minimizes cost and satisfies nutritional constraints given
+    their selected food items. Output includes optimal cost, food items, and number of servings per food item for the optimal daily diet",style="font-size:15px"),
+
+  p("The nutrients considered are:
+    Calories, Cholesterol, Total Fat, Sodium, Carbohydrates, Dietary Fiber, Protein, Vitamin A, Vitamin C, Calcium, Iron.", style="font-size:15px"),
+
+  h6("Method",style="font-weight:bold"),
+  p("The solver uses a Simplex method algorithm for minimization. The variable to be minimized is the daily total cost of the diet. The constraints are for the minimum
+    and maximum nutrients and servings per food item.", style="font-size:15px"),
+)
+
+modal_dev <-modalDialog(
+
+  h6("About the Developer", style="font-weight:bold"),
+
+  p("This diet problem solver was created by Adrianne D. Solis from BS Statistics Batch 2022 during A.Y. '24'-25
+    as a requirement for CMSC 150 under Sir Jamlech Iram N. Gojo-Cruz.",style="font-size:15px"),
+)
+
+modal_how <-modalDialog(
+
+  h6("How to Use?", style="font-weight:bold"),
+  p("Select your preferred food items in the sidebar on the left. You may
+    click the 'Select/Unselect All' button to reset your choices."),
+
+  p("Once done choosing, click the 'Find my optimal diet!' button to obtain your optimal cost and daily diet. Results
+    will be shown on the page."),
+
+  p("For a full-screen view, expand the results card (lower right button)",style="font-size:15px; font-weight:bold"),
+)
+
+
+# [3] Control inputs and outputs
+server <- function(input, output, session) {
+
+ # observe({    output$food_table = renderTable(foodServingSizes, striped=TRUE)
+  observe({output$ini_tableau_title = renderText("Simplex method process will appear here.")
+           output$final_values_txt = renderText("Your results will appear here.")})
+
+  # select/unselect food items
+  observe({
+    if(input$select_all == 0) return(NULL)
+    else if (input$select_all%%2 == 0)      # input$select_all = 1 when first selected all, so multiple of 2 -> need to unselect
+    { updateCheckboxGroupInput(session,"checkbox_group","", choiceNames = foodNamesList, choiceValues = foodValuesList) }
+    else
+    { updateCheckboxGroupInput(session,"checkbox_group","", choiceNames = foodNamesList, choiceValues = foodValuesList, selected=foodValuesList) }
+  })
+
+  observeEvent(input$test, {updateCheckboxGroupInput(session,"checkbox_group","", choiceNames = foodNamesList, choiceValues = foodValuesList, selected=1:20)})
+  observeEvent(input$about_btn, {showModal(modal_abt) })
+  observeEvent(input$dev_btn, {showModal(modal_dev) })
+  observeEvent(input$how_btn, {showModal(modal_how) })
+
+  # submit food items
+  observeEvent(input$submit,
+               if (length(input$checkbox_group)==0) {
+                 output$infeasible_txt = renderText("ERROR: You selected 0 food items.")
+                 output$final_values_txt = renderText("")
+                 output$optCost = renderText("")
+                 output$final_table = NULL
+
+                 output$basic_sol_title = renderText("")
+                 output$basic_table = NULL
+
+                 # update value boxes
+                 output$iterVal = renderText( "" )
+                 output$costVal = renderText( "" )
+                 output$foodVal = renderText( "" )
+
+               output$ini_tableau_title = renderText("")
+               output$ini_table = NULL
+               output$fin_tableau_title = renderText("")
+               output$fin_table = NULL
+
+               output$simplex_text = renderText("")
+               output$simplex_outputs = NULL }
+               else {
+               submit() })
+
+  submit <- function() {
+
+    foodVector = as.numeric(input$checkbox_group)
+
+    # Solve
+    sol = solveDiet(foodVector)
+    print(sol)
+
+    if (sol$feasible == 0) {
+      output$infeasible_txt = renderText("The problem is infeasible.
+                                           It is not possible to meet the
+                                           nutritional constraints with the selected food items.")
+
+      output$final_values_txt = renderText("")
+      output$optCost = renderText("")
+      output$final_table = NULL
+      output$fin_table = NULL
+
+
+      output$basic_sol_title = renderText("")
+      output$basic_table = NULL
+
+      # update value boxes
+      output$iterVal = renderText( sol$count )
+      output$costVal = renderText( "NA" )
+      output$foodVal = renderText( "NA" )
+
+    } else {
+      solutionTable = sol$finalValues
+      output$infeasible_txt = renderText("")
+
+      foods = nrow(solutionTable)
+
+      # final tableau
+      finalTableau = sol$finalTableau
+
+      output$fin_tableau_title = renderText("Final Tableau:")
+      output$fin_table = renderTable(finalTableau, striped = TRUE)
+
+      # Final Basic solution
+      basicSolution = sol$basicSolution
+      costRound = round(sol$optimumCost,2)
+
+      output$basic_sol_title = renderText("Final Basic solution:")
+      output$basic_table = renderTable(basicSolution, striped = TRUE)
+
+      output$optCost = renderText( paste("The cost of this optimal diet is $", costRound , " per day"))
+      output$final_values_txt = renderText("Your optimal food servings and cost:")
+      output$final_table = renderTable(solutionTable, striped = TRUE)
+
+      # update value boxes
+      output$iterVal = renderText( iterCount )
+      output$costVal = renderText( costRound )
+      output$foodVal = renderText( foods )
+    }
+
+    iterCount = sol$count
+
+    # Initial tableau and iterations
+    initialTableau = sol$initialTableau
+    iterations = sol$iterations
+
+    output$ini_tableau_title = renderText("Initial Tableau:")
+    output$ini_table = renderTable(initialTableau, striped = TRUE)
+
+    output$simplex_text = renderText("\nSimplex method iterations: ")
+    output$simplex_outputs = renderTable(iterations)
+  }
+
+}
+
+# [4] Run Program
+shinyApp(ui = ui, server = server)
+
+
+
---
+++ simplex_solver.R
@@ -0,0 +1,218 @@
+#####     CMSC 150 FINAL PROJECT         #####
+#####     Author: Adrianne D. Solis      #####
+#####     Section: B1L                   #####
+#####     Diet Problem Solver: Simplex   #####
+
+# [1] Import Data
+
+food_data <- read_excel("foodtable.xlsx")   # nutritional data and prices per food item
+#View(food_data)
+
+# food names
+foodNames = food_data$Foods
+
+# convert food names and respective row numbers (Vectors) into lists
+foodNamesList = as.list(foodNames)
+foodValuesList = as.list(1:64)
+
+# serving sizes per food items
+foodServingSizes = cbind(food_data$Foods, food_data$`Serving Size`)
+colnames(foodServingSizes) = c("Food Item","Serving Size")
+
+# minimum and maximum nutrients needed
+min_nutrient = c(1900,0,0,0,0,25,50,5000,50,800,10)
+max_nutrient = c(2250, 300, 65, 2400, 300, 100, 100, 50000, 20000, 1600, 30)
+maxServing = 10 # maximum servings
+
+# [2] Formulate objective function & constraints
+
+## sample only; depends on input by user
+# foodVector = 1:20                            # vector containing indices of selected food items
+
+createDualProblemMatrix <- function(foodVector, food_data, maxServing, min_nutrient, max_nutrient) {
+  n = length(foodVector)
+  cost_vector = c(food_data$`Price/Serving`[  foodVector  ],1)
+
+  # coefficients of the objective function
+  objectiveCoeff = cost_vector                 # vector containing price/cost per food serving
+  objectiveCoeff = as.matrix(objectiveCoeff)
+
+  # Setting up the dual problem tableau
+
+  # [2.a] Constraint set 1: nutrientj1*x1 + ... + nutrientjn*xn > min_Nutrientj
+
+  food_nutrients_mtx = food_data[  foodVector, 4:14  ]; food_nutrients_mtx = as.matrix(food_nutrients_mtx)
+  min_nutrients_mtx = rbind(food_nutrients_mtx, min_nutrient)
+
+  # [2.b] Constraint set 2: nutrientj1*x1 + ... + nutrientjn*xn < max_Nutrientj
+  #                         -> -(nutrientj1*x1 + ... + nutrientjn*xn) > -(max_Nutrientj)
+
+  max_nutrients_mtx = rbind(-food_nutrients_mtx, -max_nutrient)
+
+  # [2.c] Constraint set 3: xi < max_serving       for all i = 1,...,n
+  #                         -> -xi > -max_serving
+
+  max_serving_matrix = matrix(0, nrow=n, ncol=n); diag(max_serving_matrix) = -1
+  max_serving_matrix = rbind(max_serving_matrix, rep(-maxServing,n)) # 10 as max food serving
+  max_serving_matrix = as.matrix(max_serving_matrix)
+
+  # [2.c] Dual Problem Matrix
+  dual_mtx = cbind(min_nutrients_mtx, max_nutrients_mtx, max_serving_matrix, objectiveCoeff)
+  colnames(dual_mtx) = NULL; rownames(dual_mtx) = NULL
+
+  lis = list( cost_vector = cost_vector, dual_mtx = dual_mtx )
+  return(lis)
+}
+
+# [3] Set up tableau
+
+tableau <- function(dual_mtx) {
+  # matrix for slack variables
+  idenMtx = matrix(0, nrow=nrow(dual_mtx), ncol=nrow(dual_mtx)); diag(idenMtx) = 1;
+
+  tableau = cbind(dual_mtx[,1:(ncol(dual_mtx)-1)], idenMtx, dual_mtx[,ncol(dual_mtx)]) # form the tableau
+  tableau[nrow(tableau),ncol(tableau)] = 0
+  tableau[nrow(tableau),-(ncol(tableau)-1)] = (-1)*(tableau[nrow(tableau),-(ncol(tableau)-1)]) # negate coefficients in last row
+  return(tableau)
+   # tableau      # initial tableau
+}
+
+# [4] Function definitions for simplex solver
+
+Simplex <- function(tableau) {
+
+  iterations = data.frame()
+  row = rep("",ncol(tableau)-1)
+  cnt = 0
+  solution = c()
+
+  # assign number of rows and columns to variables
+  nrow = nrow(tableau)
+  ncol = ncol(tableau)
+
+  while (all( tableau[nrow,-ncol] >= 0) == FALSE) { # check if there are negative numbers in the last row (Except solution column)
+
+    lastRow = tableau[nrow,]
+
+    # [1] select pivot column
+    pc = which.min(tableau[nrow,])
+
+    # [2] select pivot row
+    # find test ratios
+    min = -1
+    for (i in 1:(nrow-1)) {
+      if (tableau[i,pc] > 0) {   # skip negative elements in pivot colummn
+        if (min == -1 || tableau[i,ncol]/tableau[i,pc] < min) {
+          min = tableau[i,ncol]/tableau[i,pc]
+          pr = i
+        }
+      }
+    }
+
+    # indicates that pivot row was not found (elements in pivot column are <= 0)
+    if (min == -1) {
+      return(list(feasible=0, finalTableau=tableau,Z=tableau[nrow,ncol], iterations = iterations, count = cnt))
+    }
+
+    # [3] select pivot element
+    pe <- tableau[pr,pc]
+
+    # [4] normalize pivot row
+    tableau[pr,] = (tableau[pr,])/pe
+
+    # [5] "eliminate" elements
+    for (i in 1:nrow) {
+
+      if (i == pr) {next}  # skip the pivot element
+      tv = tableau[i,pc]*(tableau[pr,])     # calculate temporary vector ( multiplier * pivot row ) wherein the multiplier is the element we want to be 0
+      tableau[i,] = tableau[i,] - tv        # update matrix row i
+
+    }
+
+    # basic solution per iteration
+    for (i in 1:(ncol-1)) {  # iterate through columns and check for solutions
+      hasSolution = FALSE
+      for (j in 1:nrow) {
+        if (tableau[j,i] != 0) { # check for nonzero value in column
+          if (hasSolution == FALSE) {
+            hasSolution = TRUE
+            solRow = j
+          } else if (hasSolution == TRUE) { # more than one nonzero value in the column, solution for the variable = 0
+            hasSolution = FALSE
+            break
+          }
+        }
+      }
+
+      if (hasSolution == TRUE) { # add solution to solution vector
+        solution = c(solution, tableau[solRow,ncol]/tableau[solRow,i])
+      } else {
+        solution = c(solution, 0)
+      }
+    }
+
+    cnt = cnt + 1
+    iterations = rbind(iterations, c(paste("Iteration", cnt),row), c("",row), tableau, c("Basic solution:", row), c(solution), c("",row))
+
+  }
+
+  # set up the basic solution matrix
+  basicSol = matrix(tableau[nrow,-(ncol-1)], nrow=1)
+  colnames(basicSol) = colnames(tableau[,-ncol])
+
+  lis = list(feasible=1, finalTableau=tableau, basicSolution=basicSol, Z=tableau[nrow,ncol], iterations = iterations, count = cnt)
+  return(lis)
+}
+
+# [5] Obtain data frame with Food items, Serving sizes, Cost
+
+solveDiet <- function(foodVector) {
+
+  # create tableau
+  dual = createDualProblemMatrix(foodVector, food_data, maxServing, min_nutrient, max_nutrient)
+  cost_vector = dual$cost_vector
+  dual_mtx = dual$dual_mtx
+  tableau = tableau(dual_mtx)
+  n = length(foodVector)
+
+  # solve using simplex method
+  simplexSol = Simplex(tableau)
+
+  # setup column names
+  colNames = c( colNames_cons = c( paste('S',1:(22+n),sep="") ),
+                colNames_vars = c( paste('X',1:n, sep="") ), 'Z', 'Solution')
+  colnames(tableau) = colNames
+
+  if (simplexSol$feasible == 0) {
+    colnames(simplexSol$iterations) = colNames;   colnames(tableau) = colNames
+    lis = list(feasible=0, initialTableau = tableau, finalTableau = simplexSol$finalTableau,
+               basicSolution = simplexSol$basicSolution, optimumCost = simplexSol$Z, iterations = simplexSol$iterations, count = simplexSol$count)
+    return(lis)
+  }
+
+
+  # format final table of serving sizes and cost per food item
+  servingSizes = simplexSol$basicSolution[(22+n+1):(22+n+1+n-1)]
+
+  foodCost = servingSizes*cost_vector[-length(cost_vector)]
+
+  foodVector_final = which(servingSizes>0)
+
+  finalValues = cbind(servingSizes, foodCost); finalValues = finalValues[ finalValues[,1]>0, ]
+  finalValues = data.frame(   food_data$Foods[foodVector[foodVector_final]], finalValues, food_data$`Serving Size`[foodVector[foodVector_final]])
+
+  colnames(finalValues) = c("Food", "Number of Servings", "Cost ($)", "Size PER 1 Serving")
+  finalTableau = simplexSol$finalTableau
+
+  colnames(finalTableau) = colNames; colnames(simplexSol$iterations) = colNames; colnames(simplexSol$basicSolution) = colNames[-(n-1)]
+
+  lis = list(feasible=1, finalValues = finalValues, initialTableau = tableau, finalTableau = finalTableau,
+             basicSolution = simplexSol$basicSolution, optimumCost = simplexSol$Z, iterations = simplexSol$iterations, count = simplexSol$count)
+
+  return(lis)
+
+}
+
+
+
+
